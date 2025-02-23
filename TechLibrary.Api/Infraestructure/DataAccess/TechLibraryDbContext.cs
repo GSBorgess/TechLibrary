f65f4454ae280b7afe531c97db3d8741a718884b
Gabriel <gabrielborgesschmidt@gmail.com>
2025-02-23 18:43:55 -0300

    first commit

---
+++ TechLibrary.Api/Infraestructure/DataAccess/TechLibraryDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechLibrary.Api.Domain.Entities;
+
+namespace TechLibrary.Api.Infraestructure.DataAccess
+{
+    public class TechLibraryDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Checkout> Checkouts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\gabri\\Documents\\Programação\\Projetos C#\\NLW-connect\\TechLibraryDb.db");
+        }
+    }
+}
